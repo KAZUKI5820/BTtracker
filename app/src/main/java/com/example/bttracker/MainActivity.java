0f2a3b1e552c37ccc8d90e44500a7184445a966a
KAZUKI5820 <65375872+KAZUKI5820@users.noreply.github.com>
2020-05-20 22:50:44 +0900

    full task comp

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bttracker;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,4 +18,21 @@ public class MainActivity extends AppCompatActivity {
         Intent toLog = new Intent(this,Log_Activity.class);
         startActivity(toLog);
     }
-}
+
+    public void goToNormal_Activity(View view) {
+        Intent toNormal = new Intent(this,Normal_Activity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanism_Activity(View view) {
+        Intent toMechanism = new Intent(this,Mechanism_Activity.class);
+        startActivity(toMechanism);
+    }
+
+    public void goToWeb1(View view){
+        Intent toWeb1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+
+         startActivity(toWeb1);
+    }
+
+
---
+++ app/src/main/java/com/example/bttracker/Mechanism_Activity.java
@@ -0,0 +1,13 @@
+package com.example.bttracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mechanism_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanism_);
+    }
+}
