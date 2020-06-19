dc07c3a682c1ee13aecf077c0f20c8f59af01b69
KAZUKI5820 <65375872+KAZUKI5820@users.noreply.github.com>
2020-06-19 22:00:18 +0900

    implemented ScrollView in NormalActivity

--- app/src/main/java/com/example/bttracker/Normal_Activity.java
+++ app/src/main/java/com/example/bttracker/Normal_Activity.java
@@ -2,6 +2,7 @@ package com.example.bttracker;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,7 +13,10 @@ public class Normal_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_);
     }
-
+    public void normalToHome(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
     public void goToNormal_Activity(View view) {
     }
 }
